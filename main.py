22d500be0448f9ea896dce576bcb9741a1950b85
Javier Quispe Varillas <jjqv1972@gmail.com>
2026-06-14 22:57:14 -0500

    trabajo7b

--- main.py
+++ main.py
@@ -1,55 +1,20 @@
-import joblib
-import numpy as np
-import sklearn
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from database import engine
+from models import Base
 
-model = joblib.load('./model/model.pkl')
-sc_x = joblib.load('./model/scaler_x.pkl')
-sc_y = joblib.load('./model/scaler_y.pkl')
 
-def predict_charges(smoker, age, bmi):
-    """
-    Predice los cargos de seguro para nuevos valores.
+app = FastAPI(
+    title="Insurance API con FastAPI",
+    description="API para predicción de primas de seguro usando Machine Learning, FastAPI y SQLAlchemy",
+    version="1.0.0"
+)
 
-    Args:
-        smoker (int): 0 for 'no' smoker, 1 for 'yes' smoker.
-        age (int): Age of the individual.
-        bmi (float): BMI of the individual.
 
-    Returns:
-        float: Predicted insurance charges.
-    """
-    new_data = np.array([[smoker, age, bmi]])
-    scaled_new_data = sc_x.transform(new_data)
-    scaled_prediction = model.predict(scaled_new_data)
-    prediction = sc_y.inverse_transform(scaled_prediction)
-
-    return round(prediction[0][0],2)
-
-app = FastAPI()
-
-#schema validador
-class Insurance(BaseModel):
-    smoker: int
-    age: int
-    bmi: float
-    
 @app.get("/")
-def home():
-    return {"message":"Insurance API"}
-
-@app.post("/insurance_price")
-def insurance_price(insurance: Insurance):
-    smoker = insurance.smoker
-    age = insurance.age
-    bmi = insurance.bmi
-    charges = predict_charges(smoker,age,bmi)
-    
-    return{
-        "smoker": smoker,
-        "age": age,
-        "bmi": bmi,
-        "charges": charges
+def index():
+    return {
+        "title": "FASTAPI INSURANCE API VERSION 1.0",
+        "message": "Bienvenido a mi API"
     }
     
+Base.metadata.create_all(engine)
